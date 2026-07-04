f69697488cc9212095f4125f1cf9f72ad6b077e1
raila-shaukat <raila.shaukat@gmail.com>
2026-07-04 14:56:13 +0500

    Set up express server and project structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Student Task Manager</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
